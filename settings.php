24c9c31ada112abe48eca15e3983c573fd44f0bc
Vinícius Moura <30481009+viniciusmouraesilva@users.noreply.github.com>
2018-06-13 12:10:43 -0300

    Update settings.php

--- settings.php
+++ settings.php
@@ -2,6 +2,6 @@
 $config['displayErrorDetails'] = true;
 $config['db']['host'] = 'localhost';
 $config['db']['user'] = 'root';
-$config['db']['pass'] = 'Zuruca@052';
+$config['db']['pass'] = 'suasenha';
 $config['db']['dbname'] = 'controleFinanceiro';
 $config['db']['charset'] = 'utf8';
